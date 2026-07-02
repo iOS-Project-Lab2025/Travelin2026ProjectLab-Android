e2bbf6954bc6eec9b6f57aa3dd48a0205c8cefff
JOSE LUIS RIQUELME <115045692+JoseLuisRiquelme@users.noreply.github.com>
2026-07-02 17:02:53 -0400

    Jl riquelme scrum 66 ho us5 create nav bar (#22)
    * home module creation
    
    * home module creation
    
    # Conflicts:
    #	core/presentation/design_system/src/main/java/com/softserveacademy/core/presentation/design_system/Color.kt
    #	settings.gradle.kts
    
    * working in the ui of the home screen
    
    # Conflicts:
    #	app/src/main/java/com/softserveacademy/travelin2026projectlab/ui/theme/Color.kt
    #	app/src/main/java/com/softserveacademy/travelin2026projectlab/ui/theme/Theme.kt
    
    * rebasing with main branch from naxPR
    
    * modifying setting gradle to add feature:home
    
    * Rearrangement of the home module
    
    * Rearrange of the Home Module
    
    * Rearrangement of the module home
    
    * home module creation
    
    * home module creation
    
    # Conflicts:
    #	core/presentation/design_system/src/main/java/com/softserveacademy/core/presentation/design_system/Color.kt
    #	settings.gradle.kts
    
    * working in the ui of the home screen
    
    # Conflicts:
    #	app/src/main/java/com/softserveacademy/travelin2026projectlab/ui/theme/Color.kt
    #	app/src/main/java/com/softserveacademy/travelin2026projectlab/ui/theme/Theme.kt
    
    # Conflicts:
    #	app/src/main/java/com/softserveacademy/travelin2026projectlab/MainActivity.kt
    
    * rebasing with main branch from naxPR
    
    * modifying setting gradle to add feature:home
    
    * Rearrangement of the home module
    
    * Rearrange of the Home Module
    
    * Rearrangement of the module home
    
    * Remove build files from tracking

---
+++ core/presentation/design_system/src/main/java/com/softserveacademy/core/presentation/design_system/Color.kt
@@ -0,0 +1,5 @@
+package com.softserveacademy.core.presentation.design_system
+
+import androidx.compose.ui.graphics.Color
+
+//val primaryColor = Color("HEXVALUE")
---
+++ feature/home/presentation/src/main/java/com/softserveacademy/home/presentation/ui/screens/TravelHomeScreen.kt
@@ -0,0 +1,60 @@
+package com.softserveacademy.home.presentation.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.softserveacademy.core.presentation.design_system.theme.Travelin2026ProjectLabTheme
+import com.softserveacademy.home.presentation.ui.components.TravelBackground
+import com.softserveacademy.home.presentation.ui.components.TravelCard
+import com.softserveacademy.home.presentation.ui.components.TravelIconsCard
+import com.softserveacademy.home.presentation.ui.components.TravelNavigationBar
+import com.softserveacademy.home.presentation.ui.components.TravelTextField
+@Composable
+fun TravelHomeScreen(modifier: Modifier = Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+       // floatingActionButton = { TravelFab() },
+        bottomBar = { TravelNavigationBar() }) { innerPadding ->
+        Box(modifier = modifier.fillMaxSize().padding(innerPadding)) {
+            TravelBackground()
+            Column(
+                modifier = Modifier
+                    .padding(20.dp),
+            ) {
+                Spacer(Modifier.height(16.dp))
+                TravelTextField()
+                Spacer(Modifier.height(16.dp))
+                TravelCard()
+                Spacer(Modifier.height(16.dp))
+                Spacer(Modifier.height(16.dp))
+                TravelIconsCard()
+
+            }
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+private fun TravelHomeScreenPreview() {
+    Travelin2026ProjectLabTheme() {
+        TravelHomeScreen()
+    }
+}
