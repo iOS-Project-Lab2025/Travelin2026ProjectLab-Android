ca99b4665fbbbee4a8cc1f8a26883a6ce98fc751
Jose Luis Riquelme <jose.riquelmes@usach.cl>
2026-07-12 17:28:19 -0400

    hot fix 2 solving missing importings

--- feature/home/presentation/src/main/java/com/softserveacademy/home/presentation/state/HomeUiState.kt
+++ feature/home/presentation/src/main/java/com/softserveacademy/home/presentation/state/HomeUiState.kt
@@ -1,15 +1,15 @@
 package com.softserveacademy.home.presentation.state
 
-import com.softserveacademy.home.domain.model.Destination
-import com.softserveacademy.home.domain.model.Hotel
-import com.softserveacademy.home.domain.model.UpcomingTrip
-import com.softserveacademy.home.domain.model.UserProfile
+import com.softserveacademy.core.domain.model.Destination
+import com.softserveacademy.core.domain.model.HomeHotel
+import com.softserveacademy.core.domain.model.UpcomingTrip
+import com.softserveacademy.core.domain.model.UserProfile
 
 
 data class HomeUiState(
     val userProfile: SectionState<UserProfile> = SectionState.Loading,
     val upcomingTrip: SectionState<UpcomingTrip> = SectionState.Loading,
     val journeyTogether: SectionState<List<Destination>> = SectionState.Loading,
-    val hotelsRecommended: SectionState<List<Hotel>> = SectionState.Loading,
+    val hotelsRecommended: SectionState<List<HomeHotel>> = SectionState.Loading,
     val searchQuery: String = ""
 )
