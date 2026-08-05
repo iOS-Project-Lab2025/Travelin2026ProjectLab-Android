58d3d9b02cc6e75f38b3abdfb0e92f5316889497
Ignacia Astrea <svt.nova@proton.me>
2026-08-05 10:03:31 -0400

    build: fix CI by adding missing baseUrl and updating plugins

--- build-logic/src/main/kotlin/Flavor.kt
+++ build-logic/src/main/kotlin/Flavor.kt
@@ -5,21 +5,9 @@ enum class FlavorDimension {
 enum class TravelinFlavor(
     val dimension: FlavorDimension,
     val applicationIdSuffix: String? = null,
-    val baseUrl: String
+    val baseUrl: String = "https://private-amnesiac-923781-travelin1.apiary-mock.com/"
 ) {
-    dev(
-        FlavorDimension.contentType,
-        ".dev",
-        "https://private-amnesiac-923781-travelin1.apiary-mock.com/"
-    ),
-    staging(
-        FlavorDimension.contentType,
-        ".staging",
-        "https://private-amnesiac-923781-travelin1.apiary-mock.com/"
-    ),
-    prod(
-        FlavorDimension.contentType,
-        null,
-        "https://private-amnesiac-923781-travelin1.apiary-mock.com/"
-    )
+    dev(FlavorDimension.contentType, ".dev"),
+    staging(FlavorDimension.contentType, ".staging"),
+    prod(FlavorDimension.contentType)
 }
